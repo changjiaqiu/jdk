9766caa5841e1fe441bf50439b755c6babea18be
changjiaqiu <1311572051@qq.com>
2025-09-17 16:18:43 +0800

    feat：Support TableSwitch
    Signed-off-by: changjiaqiu <1311572051@qq.com>

--- test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/TestTableSwitch.java
+++ test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/TestTableSwitch.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2025, the Jeandle-JDK Authors. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 /*
  * @test
@@ -16,21 +35,8 @@ import jdk.test.lib.Asserts;
 
 public class TestTableSwitch {
     public static void main(String[] args) throws Exception {
-        preInit();
         testBasicBoundaryScenarios();
         testLargeRangeSwitch();
-    }
-
-    // Pre-initialize required classes (consistent with TestTypeConversion)
-    private static void preInit() throws Exception {
-        Class.forName("java.lang.Byte");
-        Class.forName("java.lang.Character");
-        Class.forName("java.lang.Short");
-        Class.forName("java.lang.Integer");
-        Class.forName("java.lang.Long");
-        Class.forName("java.lang.Float");
-        Class.forName("java.lang.Double");
-        Class.forName("jdk.test.lib.Asserts");
     }
 
     public static int minPositiveSwitch(int num) {
